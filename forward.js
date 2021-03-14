425df73ce57442d296b5ba22bac3c83055397597
xuopoj <57469819+xuopoj@users.noreply.github.com>
2021-03-15 04:13:49 +0800

    Update forward.js

--- forward.js
+++ forward.js
@@ -1,7 +1,7 @@
 var body = $response.body; 
 console.log(body);
 var obj = JSON.parse(body);
-obj.data.data.time = obj.data.data.time + 130; 
+obj.data.data.time = obj.data.data.time + 60; 
 body = JSON.stringify(obj); 
 console.log(body); 
 $done(body);
