9e5691a9d68148bb76ac77db71342a1ac1769c0c
xuopoj <57469819+xuopoj@users.noreply.github.com>
2021-03-15 16:41:03 +0800

    Update delay.js

--- delay.js
+++ delay.js
@@ -1,5 +1,21 @@
-console.log('before' + new Date());
-setTimeout(()=>{
-  console.log('after' + new Date());
-  $done({});
-}, 5600);
+// 20 milliseconds to 8
+var OFFSET = 20; 
+
+var now = new Date();
+
+var eight = new Date();
+eight.setHours(16);
+eight.setMinutes(43);
+eight.setSeconds(0);
+
+var delay = eight - now - OFFSET;
+
+if(delay <=0){
+    console.log('no delay')
+    $done({});
+}else{
+    setTimeout(function(){
+        console.log('delay for '+ delay +' milliseconds')
+        $done({});
+    }, delay)
+}
