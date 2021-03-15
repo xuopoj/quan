4107b20d02703f597a43fe70d211cc7ba8c84ada
xuopoj <57469819+xuopoj@users.noreply.github.com>
2021-03-15 17:13:46 +0800

    Update delay.js

--- delay.js
+++ delay.js
@@ -1,12 +1,18 @@
+
 // 20 milliseconds to 8
 var OFFSET = 20; 
 
 var now = new Date();
 
 var eight = new Date();
-eight.setHours(16);
-eight.setMinutes(43);
+eight.setHours(17);
+eight.setMinutes(15);
 eight.setSeconds(0);
+eight.setMilliseconds(0);
+
+console.log('now: ' + now + 'TTT milliseconds' + now.getMilliseconds());
+
+console.log('wait until: ' + eight + 'TTT milliseconds' + eight.getMilliseconds());
 
 var delay = eight - now - OFFSET;
 
